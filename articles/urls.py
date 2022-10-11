6e4dde946702970df94fe03b96889ea98e6189c3
Lucas Fonseca <lucasfreireri@gmail.com>
2022-10-11 07:43:20 -0300

    Added articles template and view

--- articles/urls.py
+++ articles/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'articles'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('articles/', views.articles, name='articles'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from .models import Article
+
 def index(request):
     # Main page 
     return render(request, 'articles/index.html')
+
+def articles(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/articles.html', context)
