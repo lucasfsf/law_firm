b48b34edeb3e02b36e088d976e07ee9dcb730847
Lucas Fonseca <lucasfreireri@gmail.com>
2022-10-14 10:29:20 -0300

    Changed links to point to account and lawsuits

--- law_firm/settings.py
+++ law_firm/settings.py
@@ -132,5 +132,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = "users:dashboard"
-LOGOUT_REDIRECT_URL = "users:dashboard"
+LOGIN_REDIRECT_URL = "lawsuits:lawsuits"
+LOGOUT_REDIRECT_URL = "articles:index"
