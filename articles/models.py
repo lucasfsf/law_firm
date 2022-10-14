553e18e3e6b33bdf3357bfae0e894732294f4ade
Lucas Fonseca <lucasfreireri@gmail.com>
2022-10-14 06:33:43 -0300

    Added law type to article

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,6 @@
+from email.policy import default
 from random import choices
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 class Article(models.Model):
@@ -6,5 +8,33 @@ class Article(models.Model):
     content = models.TextField()
     date_added = models.DateField(auto_now_add=True)
 
+    #Law Choices
+    CONSTITUTIONAL = "CO"
+    TAX = "TR"
+    CIVIL = "CI"
+    ADMINISTRATIVE = "AD"
+    CRIMINAL = "CR"
+    CORPORATE = "CP"
+    FAMILY = "FA"
+    LABOR = "LA"
+    OTHERS = "OT"
+
+    TRIBUTARY_LAW_CHOICES = [
+        (CONSTITUTIONAL, "Constitucional"),
+        (TAX, "Tributário"),
+        (CIVIL, "Civil"),
+        (ADMINISTRATIVE, "Administrativo"),
+        (CRIMINAL, "Criminal"),
+        (CORPORATE, "Corporativo"),
+        (FAMILY, "de Familia"),
+        (LABOR, "Trabalhista"),
+        (OTHERS, "(Outros)"),
+    ]
+
+    law_type = models.CharField(max_length=2, 
+        choices=TRIBUTARY_LAW_CHOICES, 
+        default=OTHERS,
+        )
+        
     def __str__(self):
         return self.title
