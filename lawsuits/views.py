e6e8421e06592cd56026c7c2124d1c209c15b4dc
Lucas Fonseca <lucasfreireri@gmail.com>
2022-10-14 12:08:00 -0300

    Lawsuit card showing message when user has no registered lawsuit

--- lawsuits/views.py
+++ lawsuits/views.py
@@ -3,15 +3,14 @@ from django.contrib.auth.decorators import login_required
 
 from .models import Lawsuit
 
-#TODO - add login required decorator
 
 @login_required
 def lawsuits(request):
+    context = {}
     if request.user.is_authenticated:
         lawsuits = Lawsuit.objects.filter(customer=request.user)
         context = {
             "lawsuits": lawsuits,
         }
-    else:
-        return redirect("users:login")
+
     return render(request, "lawsuits/lawsuits.html", context)
