5a506f2fea774449d3f339d7839167c155a28ef3
Lucas Fonseca <lucasfreireri@gmail.com>
2022-10-27 08:09:53 -0300

    Putting all company and site variables in a single file to facilitate use by non technical users

---
+++ articles/company_fields.py
@@ -0,0 +1,28 @@
+"""Change the fields below to update site to your law firm"""
+# Company Data
+company_name = 'Law Firma'
+company_description = 'A webapp template to be used by lawfirms where you can publish' \
+            'articles and your clients will be able to keeptrack of their lawsuits in a' \
+            'friendly, non-lawyery language.'
+
+# Navigation
+articles_title = 'Articles'
+law_suits_title = 'Lawsuits'
+contact_title = 'Contact'
+account_title = 'Account'
+logout_title = 'Log out'
+login_title = 'Log in'
+
+
+# Forms
+
+default_company_values = {
+    'company_name': company_name,
+    'company_description': company_description,
+    'articles_title': articles_title,
+    'law_suits_title': law_suits_title,
+    'contact_title': contact_title,
+    'account_title': account_title,
+    'logout_title': logout_title,
+    'login_title': login_title,
+}
--- articles/views.py
+++ articles/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 
 from .models import Article
 
+from .company_fields import default_company_values
+
+context = default_company_values
+
 def index(request):
     # Main page 
-    return render(request, 'articles/index.html')
+    return render(request, 'articles/index.html', context)
 
 def articles(request):
     # Checks if something was searched
@@ -14,15 +18,12 @@ def articles(request):
     else:
         search_term = request.GET['q']
         articles = Article.objects.filter(content__icontains=search_term).order_by('-date_added')
-    context = {
-        'articles': articles,
-    }
+    context['articles'] = articles
     return render(request, 'articles/articles.html', context)
 
 def article(request, article_id):
     # Shows a single, full, article
     article = Article.objects.get(id=article_id)
-    context = {
-        "article": article,
-    }
+    context['article'] = article
+
     return render(request, "articles/article.html", context)
