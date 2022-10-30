0d571d796b2dc1d62057cbae24f404e1d1a95310
Lucas Fonseca <lucasfreireri@gmail.com>
2022-10-30 07:41:12 -0300

    Changed article admin to display more attributes and added search field

--- articles/admin.py
+++ articles/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Article
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['law_type', 'title', 'slug']
+    search_fields = ['law_type', 'title', 'content']
+
+admin.site.register(Article, ArticleAdmin)
